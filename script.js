157c91ae7430817c32801a07d3d3848d6e943790
Choaib ELMADI <choaibamd@gmail.com>
2025-01-07 23:44:11 +0100

    Hand written digits in a 20x20 grid

--- script.js
+++ script.js
@@ -1,16 +1,69 @@
-const GRID_WIDTH = 18;
-const GRID_HEIGHT = 18;
+const GRID_WIDTH = 20;
+const GRID_HEIGHT = 20;
 const GRID_PIXELS = GRID_WIDTH * GRID_HEIGHT;
 
 const imageGridContainer = document.getElementsByClassName(
 	"image-grid-container"
 )[0];
+const clearGridButton = document.getElementById("clear-grid");
 
-for (let i = 0; i < GRID_PIXELS; ++i) {
-	const pixelContainer = document.createElement("div");
+//! ---- ---- ---- ---- ---- !//
 
-	pixelContainer.className = "pixel-container";
-	pixelContainer.dataset.index = `pixel-${i}`;
+let pixelsContainer;
 
-	imageGridContainer.appendChild(pixelContainer);
+//! ---- ---- ---- ---- ---- !//
+
+generateImageGrid();
+fillVisitedPixels();
+clearGridButton.addEventListener("click", clearImageGrid);
+
+//! ---- ---- ---- ---- ---- !//
+
+function generateImageGrid() {
+	for (let i = 0; i < GRID_PIXELS; ++i) {
+		const pixelContainer = document.createElement("div");
+
+		pixelContainer.className = "pixel-container";
+		pixelContainer.dataset.index = `pixel-${Math.floor(i / GRID_WIDTH)}-${
+			i % GRID_WIDTH
+		}`;
+
+		imageGridContainer.appendChild(pixelContainer);
+	}
+}
+
+function fillVisitedPixels() {
+	pixelsContainer = document.getElementsByClassName("pixel-container");
+	if (!pixelsContainer) return;
+
+	let isLeftShiftPressed = false;
+
+	document.onkeydown = function (event) {
+		if (event.code === "ShiftLeft") {
+			isLeftShiftPressed = true;
+		}
+	};
+
+	document.onkeyup = function (event) {
+		if (event.code === "ShiftLeft") {
+			isLeftShiftPressed = false;
+		}
+	};
+
+	Array.prototype.forEach.call(pixelsContainer, function (pixelContainer, i) {
+		pixelContainer.onmouseover = function (target) {
+			if (isLeftShiftPressed) {
+				this.classList.add("is-visited");
+			}
+		};
+	});
+}
+
+function clearImageGrid() {
+	pixelsContainer = document.getElementsByClassName("pixel-container");
+	if (!pixelsContainer) return;
+
+	Array.prototype.forEach.call(pixelsContainer, function (pixelContainer, i) {
+		pixelContainer.classList.remove("is-visited");
+	});
 }
