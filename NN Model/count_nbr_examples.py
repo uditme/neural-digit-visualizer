557309fba1fbe3dcf7a24d8be49bac575e0a8a35
Choaib ELMADI <choaibamd@gmail.com>
2025-01-09 22:37:17 +0100

    Add additional data

---
+++ NN Model/count_nbr_examples.py
@@ -0,0 +1,17 @@
+def count_nbr_examples(file_path, feature):
+    count = 0
+
+    with open(file_path, "r") as f:
+        for line in f:
+            if line.startswith(str(feature)):
+                count += 1
+
+    return count
+
+
+file_name = ""  # EMPTY IS DEFAULT
+file_path = f"C:\\Users\\Choaib ELMADI\\Documents\\D.I.F.Y\\4. Artificial Intelligence\\A. AI Projects\\Neural Digit Visualizer\\Data\\y{file_name}.txt"
+feature = 4
+
+count = count_nbr_examples(file_path, feature)
+print(f"{count}'s {feature} features")
